a10a250c1ad10db36eca8de20fa82b424a87c431
Armin Omerbegovic <aomerbegov2@etf.unsa.ba>
2017-05-23 21:45:38 +0200

    klase

---
+++ Projekat/Teamer/Teamer/Models/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamer.Models
+{
+    public class Manager
+    {
+        List<Tim> Timovi { get; set; }
+    }
+}
---
+++ Projekat/Teamer/Teamer/Models/Tim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamer.Models
+{
+    public class Tim
+    {
+        string Naziv { get; set; }
+        byte[] Slika { get; set; }
+        Trener Trener;
+        List<Igrac> Igraci;
+        Manager Manager;
+        List<Dogadjaj> Dogadjaji;
+        //List<Image> LISTA SLIKAA
+         
+    }
+}
