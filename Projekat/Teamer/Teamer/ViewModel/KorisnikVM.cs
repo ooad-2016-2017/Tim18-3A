2bc589a7c29449a3b4d55693723a7ae9043ffd37
Amel <asazic1@etf.unsa.ba>
2017-06-07 00:17:55 +0200

    View-ovi završeni

---
+++ Projekat/Teamer/Teamer/ViewModel/KorisnikVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamer.ViewModel
+{
+    abstract class KorisnikVM
+    {
+        public DateTime DatumRodjenja { get; set; }
+        public string Email { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public byte[] Slika { get; set; }
+    }
+}
---
+++ Projekat/Teamer/Teamer/ViewModel/MenadzerVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teamer.Models;
+
+namespace Teamer.ViewModel
+{
+    class MenadzerVM : KorisnikVM
+    {
+        public List<Tim> Timovi { get; set; }
+    }
+}
